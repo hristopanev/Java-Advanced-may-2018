cd238eacd8e1e53a2358d036a02286f5982350d4
Hristo Panev <hristopanef@gmail.com>
2018-08-17 21:35:35 +0200

    Animal Farm

---
+++ Java OOP Basic Juni 2018/Exercise/Encapsulation/src/P03_animal_Farm/Main.java
@@ -0,0 +1,23 @@
+package P03_animal_Farm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = reader.readLine();
+        int age = Integer.parseInt(reader.readLine());
+
+
+        try {
+            Chicken chicken = new Chicken(name, age);
+            System.out.println(chicken.toString());
+        }catch (IllegalArgumentException ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
+}
