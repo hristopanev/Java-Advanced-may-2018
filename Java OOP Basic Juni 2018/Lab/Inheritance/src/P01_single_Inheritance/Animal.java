29862889f7a1ca91ec8e519eaf95d94d94c80f38
Hristo Panev <hristopanef@gmail.com>
2018-08-19 21:30:05 +0200

    Fragile Base Class

---
+++ Java OOP Basic Juni 2018/Lab/Inheritance/src/P01_single_Inheritance/Animal.java
@@ -0,0 +1,7 @@
+package P01_single_Inheritance;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating...");
+    }
+}
---
+++ Java OOP Basic Juni 2018/Lab/Inheritance/src/P04_fragile_Base_Class/Food.java
@@ -0,0 +1,4 @@
+package P04_fragile_Base_Class;
+
+public class Food {
+}
