1901b37a70c8bd29d3c4f333fae80eeab5aefe03
Hristo Panev <hristopanef@gmail.com>
2018-08-21 11:57:56 +0200

    Person

---
+++ Java OOP Basic Juni 2018/Exercise/Inheritance/src/P01_Person/Main.java
@@ -0,0 +1,23 @@
+package P01_Person;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = reader.readLine();
+        int age = Integer.parseInt(reader.readLine());
+
+        try {
+            Child child = new Child(name, age);
+            System.out.println(child);
+            String personClassName = Person.class.getSimpleName();
+            String chilClassName = Child.class.getSimpleName();
+        }catch (IllegalArgumentException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
