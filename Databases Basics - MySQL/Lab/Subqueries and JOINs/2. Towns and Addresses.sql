43ce2afe8abf677f317952fc9e2f67b183e79ef4
Hristo Panev <hristopanef@gmail.com>
2018-10-07 09:40:39 +0200

    Subqueries and JOINs

---
+++ Databases Basics - MySQL/Lab/Subqueries and JOINs/2. Towns and Addresses.sql
@@ -0,0 +1,11 @@
+SELECT 
+    t.town_id, t.name, a.address_text
+FROM
+    towns AS t
+        INNER JOIN
+    addresses AS a ON a.town_id = t.town_id
+WHERE
+    t.name = 'San Francisco'
+        OR t.name = 'Sofia'
+        OR t.name = 'Carnation'
+ORDER BY town_id , address_id;
---
+++ Databases Basics - MySQL/Lab/Subqueries and JOINs/3. Employees Without Managers.sql
@@ -0,0 +1,12 @@
+SELECT 
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    d.department_id,
+    e.salary
+FROM
+    employees AS e
+        JOIN
+    departments AS d ON e.department_id = d.department_id
+WHERE
+    e.manager_id IS NULL;
