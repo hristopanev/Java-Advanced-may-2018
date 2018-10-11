31a8e6348e43beeb7e9a32d46fe7c7dab322787e
Hristo Panev <hristopanef@gmail.com>
2018-10-11 16:32:56 +0200

    04. Employees from Town

---
+++ Databases Basics - MySQL/Exercises/Functions Triggers And Transactions/04. Employees from Town.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_from_town(town_name VARCHAR(50))
+BEGIN
+	SELECT
+		e.first_name,
+        e.last_name
+	FROM employees AS e
+    JOIN addresses AS a
+    ON e.address_id = a.address_id
+    JOIN towns AS t
+    ON t.town_id = a.town_id
+    WHERE t.name = town_name
+    ORDER BY first_name, last_name, employee_id;
+END $$
+
+DELIMITER ;
+CALL usp_get_employees_from_town('Berlin');
+    
