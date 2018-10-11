64a9a62ab963f756fc35bf3ef7825f6232f3aad4
Hristo Panev <hristopanef@gmail.com>
2018-10-11 16:14:35 +0200

    03. Town Names Starting With

--- Databases Basics - MySQL/Exercises/Functions Triggers And Transactions/03. Town Names Starting With.sql
+++ Databases Basics - MySQL/Exercises/Functions Triggers And Transactions/03. Town Names Starting With.sql
@@ -1,12 +1,14 @@
 DELIMITER $$
-CREATE PROCEDURE usp_get_towns_starting_with(input_alph VARCHAR(10))
+CREATE PROCEDURE usp_get_towns_starting_with(input_alph VARCHAR(20))
 BEGIN
 	SELECT
 		t.name
 	FROM towns AS t
-    WHERE LEFT(name, 1) LIKE (input_alph)
+    WHERE LEFT(name, LENGTH(input_alph)) LIKE (input_alph)
     ORDER BY t.name;
 END $$
 DELIMITER ;
+
+DROP PROCEDURE usp_get_towns_starting_with;
 
 CALL usp_get_towns_starting_with('b');
