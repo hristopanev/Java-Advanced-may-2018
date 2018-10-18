6a444e854de7bc57c026b38d4ad871654f8c4e37
Hristo Panev <hristopanef@gmail.com>
2018-10-18 14:06:46 +0200

    EXAM PREPARATION III

--- Databases Basics - MySQL/EXAM PREPARATION/EXAM PREPARATION III/03. Update.sql
+++ Databases Basics - MySQL/EXAM PREPARATION/EXAM PREPARATION III/03. Update.sql
@@ -1,14 +1,6 @@
 UPDATE repositories_contributors AS rc
-        JOIN
-    repositories AS r 
-    ON r.id NOT IN (SELECT 
-            Jc.repository_id
-    FROM
-    (SELECT 
-       *
-    FROM
-    repositories_contributors) AS Jc) 
-SET 
-    rc.repository_id = r.id
-WHERE
-    rc.repository_id = rc.contributor_id;
+JOIN repositories AS r 
+ON r.id NOT IN (SELECT  Jc.repository_id
+FROM(SELECT * FROM repositories_contributors) AS Jc) 
+SET rc.repository_id = r.id
+WHERE rc.repository_id = rc.contributor_id;
