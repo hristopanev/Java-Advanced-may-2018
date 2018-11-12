04c68a91d8d7fc7c5df4a15535af2cc359526012
Hristo Panev <hristopanef@gmail.com>
2018-11-12 15:56:53 +0100

    up

--- Databases Frameworks - Hibernate & Spring Data/WorkShop-MiniOMR/src/main/java/WorkShop/db/anotations/PrimaryKey.java
+++
@@ -1,9 +0,0 @@
-package WorkShop.db.anotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface PrimaryKey {
-    String name();
-}
