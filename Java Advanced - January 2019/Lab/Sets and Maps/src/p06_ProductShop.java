4654cd4255b9a026eafaf8e4e559026bd1ce46f2
Hristo Panev <hristopanef@gmail.com>
2019-01-24 20:07:39 +0100

    Java Advanced

--- Java Advanced - January 2019/Lab/Sets and Maps/src/p06_ProductShop.java
+++ Java Advanced - January 2019/Lab/Sets and Maps/src/p06_ProductShop.java
@@ -1,9 +1,41 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class p06_ProductShop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Map<String, LinkedHashMap<String, Double>> shops = new TreeMap<>();
+        String line = scanner.nextLine();
 
+        while (!line.equals("Revision")){
+            String[] data = line.split(", ");
+
+            String shopName = data[0];
+            String productName = data[1];
+            double productPrice = Double.parseDouble(data[2]);
+
+            LinkedHashMap<String, Double> products = new LinkedHashMap<>();
+            if (shops.containsKey(shopName)) {
+                products = shops.get(shopName);
+            }
+
+            products.put(productName, productPrice);
+            shops.put(shopName, products);
+
+
+            line = scanner.nextLine();
+        }
+
+        for (String shopName : shops.keySet()) {
+            System.out.println(shopName +"->");
+            LinkedHashMap<String, Double> products = shops.get(shopName);
+
+            for (String product : products.keySet()) {
+                System.out.printf("Product: %s, Price: %.1f%n", product, products.get(product));
+            }
+        }
     }
 }
