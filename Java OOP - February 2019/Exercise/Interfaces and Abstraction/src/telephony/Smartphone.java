f5773f52e26c367368bf869ec8c5aa4056b15b0c
Hristo Panev <hristopanef@gmail.com>
2019-03-17 11:43:15 +0100

    Interfaces and Abstraction

--- Java OOP - February 2019/Exercise/Interfaces and Abstraction/src/telephony/Smartphone.java
+++ Java OOP - February 2019/Exercise/Interfaces and Abstraction/src/telephony/Smartphone.java
@@ -1,41 +1,44 @@
 package telephony;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Smartphone implements Callable, Browsable {
     private List<String> numbers;
     private List<String> urls;
 
     public Smartphone(List<String> numbers, List<String> urls) {
-        this.numbers = new ArrayList<>();
-        this.urls = new ArrayList<>();
+        this.numbers = numbers;
+        this.urls = urls;
     }
 
     @Override
     public String browse() {
         StringBuilder sb = new StringBuilder();
-
-        for (String url : urls) {
-            if (url ==null || url.chars().anyMatch(Character::isDigit)){
-                throw new IllegalArgumentException("Invalid URL!");
+        for (String url : this.urls) {
+            if (!url.matches("[A-Za-z:\\/\\.]+")) {
+                sb.append("Invalid URL!");
+            } else {
+                sb.append("Browsing: ").append(url).append("!").append(System.lineSeparator());
             }
-            sb.append(String.format("Browsing: %s!", url)).append(System.lineSeparator());
-
         }
+
         return sb.toString();
     }
 
     @Override
-    public String  call() {
+    public String call() throws IllegalArgumentException {
         StringBuilder sb = new StringBuilder();
-        for (String number : numbers) {
-            if (number == null || !number.chars().anyMatch(Character::isDigit)){
-                throw new IllegalArgumentException("Invalid number!%n" );
+        String regex = "[0-9]+";
+        Pattern pattern = Pattern.compile(regex);
+        for (String number : this.numbers) {
+            Matcher matcher = pattern.matcher(number);
+            if (!matcher.matches()) {
+                sb.append("Invalid number!");
+            } else {
+                sb.append("Calling... ").append(number).append(System.lineSeparator());
             }
-            sb.append(String.format("Calling... %s%n", number)).append(System.lineSeparator());
         }
-
-        return  sb.toString();
+        return sb.toString();
     }
 }
