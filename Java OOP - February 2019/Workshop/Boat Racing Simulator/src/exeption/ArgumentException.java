12ed0df5f30b7ba9e18ef781e15ec6f208028d53
Hristo Panev <hristopanef@gmail.com>
2019-03-29 17:09:08 +0100

    Workshop

--- Java OOP - February 2019/Workshop/Boat Racing Simulator/src/exeption/ArgumentException.java
+++
@@ -1,4 +0,0 @@
-package exeption;
-
-public class ArgumentException extends Exception {
-}
--- Java OOP - February 2019/Workshop/Boat Racing Simulator/src/repository/BoatRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.boat.Boat;
-import model.engine.BaseRepository;
-
-public class BoatRepository extends BaseRepository<Boat> {
-}
