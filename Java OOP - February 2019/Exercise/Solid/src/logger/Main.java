684e9236124601d0dd7113f6bd5982a4757f7723
Hristo Panev <hristopanef@gmail.com>
2019-03-30 20:23:57 +0100

    Solid

--- Java OOP - February 2019/Exercise/Solid/src/logger/Main.java
+++ Java OOP - February 2019/Exercise/Solid/src/logger/Main.java
@@ -1,8 +1,12 @@
 package logger;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     }
+
 }
